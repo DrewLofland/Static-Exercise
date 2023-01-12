55728116a7c108b3d45bd014381dbb0a4cae97a0
Drew Lofland <alofland25@gmail.com>
2023-01-12 11:03:31 -0500

    finished exercise

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double tempf)
+        {
+            return (tempf - 32) * (5 / 9); 
+        }
+
+        public static double CelsiusToFahrenheit(double tempc) 
+        {
+            return (tempc * (9/5) + 32);
+        
+        }
+
+
+
+
+    }
+
+
+}
